0f156e3c69c270dd906ff446ad51be987cc2ed62
manojkunapalli <manojkunapalli@gmail.com>
2020-10-09 12:42:51 +0530

    Modified: Wage Calculation till condition reached

--- src/EmployeeWageCalculation.java
+++ src/EmployeeWageCalculation.java
@@ -18,24 +18,33 @@ public class EmployeeWageCalculation {
         int hours_per_day = 0;
         String str = "";
         int TotalSalary=0;
+        int TotalHours=0;
         Random rand = new Random();
         int employeeType;
         for(int i=1;i<=num_days;i++){
-             employeeType = 1 + rand.nextInt( 2 );
-            switch (employeeType) {
-                case 1:
-                    str="Full Time Employee";
-                    hours_per_day=8;
-                    break;
-                case 2:
-                    str="Part Time Employee";
-                    hours_per_day=4;
+            if(TotalHours <= 100) {
+                employeeType = 1 + rand.nextInt(2);
+                switch (employeeType) {
+                    case 1:
+                        str = "Full Time Employee";
+                        hours_per_day = 8;
+                        TotalHours += hours_per_day;
+                        break;
+                    case 2:
+                        str = "Part Time Employee";
+                        hours_per_day = 4;
+                        TotalHours += hours_per_day;
+                }
+                wage = (hours_per_day) * wage_per_hour;
+                TotalSalary += wage;
+                System.out.println("Salary of " + str + " on the day " + i + " = " + wage);
+            } else {
+                System.out.println("Total working hours reached 100 and above..................");
+                break;
             }
-            wage = ( hours_per_day ) * wage_per_hour;
-            TotalSalary+=wage;
-            System.out.println("Salary of "+str+" on the day "+i+" = "+wage);
         }
         System.out.println("Total Salary = "+TotalSalary);
+        System.out.println("Total Working hours = "+TotalHours);
     }
 
     public static void main(String[] args){
