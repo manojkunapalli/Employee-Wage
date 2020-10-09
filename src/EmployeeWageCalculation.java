c7e78a813c83dd89540385d1ce4a682463960cb2
manojkunapalli <manojkunapalli@gmail.com>
2020-10-09 11:22:53 +0530

    Added: Employee Attendance

--- src/EmployeeWageCalculation.java
+++ src/EmployeeWageCalculation.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 public class EmployeeWageCalculation {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Computation program");
+
+        //Employee Attendance
+        Random rand = new Random();
+        int present_absent = rand.nextInt(1);
+        if(present_absent == 1){
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
