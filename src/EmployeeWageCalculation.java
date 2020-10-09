84fd3cad5353d692a4e2c85b54d25e84553c0938
manojkunapalli <manojkunapalli@gmail.com>
2020-10-09 11:59:06 +0530

    Changed: Wage Calculation for FullTime and PartTime Employee

--- src/EmployeeWageCalculation.java
+++ src/EmployeeWageCalculation.java
@@ -2,9 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class EmployeeWageCalculation {
-    public static void check_attendance() {
-        Random rand = new Random();
-        int present_absent = rand.nextInt(1);
+    public static void check_Attendance(int present_absent) {
+
         if(present_absent == 1){
             System.out.println("Employee is Present");
         } else {
@@ -12,23 +11,37 @@ public class EmployeeWageCalculation {
         }
     }
 
-    public static void calculate_Wage() {
+    public static void calculate_Wage(int employeeType) {
         int wage;
         int wage_per_hour = 20;
-        int hours_per_day = 8;
         Scanner input = new Scanner(System.in);
-        System.out.print("number of days Employee worked = ");
-        int num_days = input.nextInt();
-        wage = ( num_days * hours_per_day ) * wage_per_hour;
-        System.out.println("Employee wage for working hours = "+wage);
+        if(employeeType == 0){
+            int hours_per_day = 8;
+            System.out.println("FullTime Employee");
+            System.out.print("number of days Employee worked = ");
+            int num_days = input.nextInt();
+            wage = ( num_days * hours_per_day ) * wage_per_hour;
+            System.out.println("Employee wage for working hours = "+wage);
+        } else {
+            System.out.println("PartTime Employee");
+            int hours_per_day = 8;
+            System.out.print("number of days Employee worked = ");
+            int num_days = input.nextInt();
+            wage = ( num_days * hours_per_day ) * wage_per_hour;
+            System.out.println("Employee wage for working hours = "+wage);
+        }
     }
 
     public static void main(String[] args){
         System.out.println("Welcome to Employee Computation program");
 
+        Random rand = new Random();
         //Employee Attendance
-        check_attendance();
+        int present_absent = rand.nextInt(1);
+        check_Attendance(present_absent);
+
         //Employee wage calculation
-        calculate_Wage();
+        int empType = rand.nextInt(1);
+        calculate_Wage(empType);
     }
 }
