757362b00ca74e83ea083217c97f5a05b65660cd
manojkunapalli <manojkunapalli@gmail.com>
2020-10-14 15:01:53 +0530

    Added: Store Daily wage and Total Wage

--- src/EmployeeWageCalculation.java
+++ src/EmployeeWageCalculation.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 interface EmployeeWageInterface {
-    double calculateWage(List<EmpWageBuilder> arraylist,int index);
+    void calculateWage(List<EmpWageBuilder> arraylist,int index);
 }
 
 class EmpWageBuilder implements EmployeeWageInterface {
@@ -9,6 +9,7 @@ class EmpWageBuilder implements EmployeeWageInterface {
     public int numOfWorkingDays;
     public double wagePerHour;
     public int workingHoursPerMonth;
+    Map<String,Double> salaryMap = new HashMap<>();
 
     EmpWageBuilder(String companyName,int numOfWorkingDays,double wagePerHour,int workingHoursPerMonth) {
         this.companyName = companyName;
@@ -17,17 +18,16 @@ class EmpWageBuilder implements EmployeeWageInterface {
         this.workingHoursPerMonth = workingHoursPerMonth;
     }
 
-    public double calculateWage(List<EmpWageBuilder> arraylist,int index) {
+    public void calculateWage(List<EmpWageBuilder> arraylist,int index) {
         double wage;
         int hoursPerDay = 0;
         String str = "";
         double TotalSalary=0;
         int TotalHours=0;
-        int fullTimeWorkingHours=0;
-        int partTimeWorkingHours=0;
         Random rand = new Random();
         int employeeType;
-        Map<String,Integer> map = new HashMap<>();
+        Map<Integer,Double> map = new HashMap<>();
+
         System.out.println("Calculating Wages for a month of "+arraylist.get(index).companyName+"............");
         for(int i=1;i<=arraylist.get(index).numOfWorkingDays;i++) {
             if(TotalHours <= arraylist.get(index).workingHoursPerMonth) {
@@ -37,39 +37,32 @@ class EmpWageBuilder implements EmployeeWageInterface {
                         str = "FullTimeEmployee";
                         hoursPerDay = 8;
                         TotalHours += hoursPerDay;
-                        fullTimeWorkingHours+=hoursPerDay;
-                        map.put(str,fullTimeWorkingHours);
                         break;
                     case 2:
                         str = "PartTimeEmployee";
                         hoursPerDay = 4;
                         TotalHours += hoursPerDay;
-                        partTimeWorkingHours+=hoursPerDay;
-                        map.put(str,partTimeWorkingHours);
                         break;
                 }
                 wage = (hoursPerDay) * arraylist.get(index).wagePerHour;
+                map.put(i,wage);
                 TotalSalary += wage;
             } else {
                 break;
             }
         }
-        System.out.println("EmployeeType                 WorkingHours");
-        for ( Map.Entry<String, Integer>  t  :  map.entrySet() )  {
-            System.out.println(t.getKey() +"     ----->      "+t.getValue());
+        System.out.println("Day                 DailyWage");
+        for ( Map.Entry<Integer, Double>  t  :  map.entrySet() )  {
+            System.out.println(t.getKey() +"            ----->      "+t.getValue());
         }
 
         System.out.println();
-        System.out.println(companyName+" Total Working hours = "+TotalHours);
-        return TotalSalary;
+        System.out.println(arraylist.get(index).companyName+" Total Working hours = "+TotalHours);
+        salaryMap.put(arraylist.get(index).companyName,TotalSalary);
     }
 }
 
-public class EmployeeWageCalculation extends EmpWageBuilder{
-    EmployeeWageCalculation(String companyName, int numOfWorkingDays, double wagePerHour, int workingHoursPerMonth) {
-        super(companyName, numOfWorkingDays, wagePerHour, workingHoursPerMonth);
-    }
-
+public class EmployeeWageCalculation {
     public static void checkAttendance() {
         System.out.println("*****Attendance Check*****");
         Random rand = new Random();
@@ -81,19 +74,14 @@ public class EmployeeWageCalculation extends EmpWageBuilder{
         }
     }
 
-    @Override
-    public String toString() {
-        return "Company name => "+companyName+"\nnumber of working days => "+numOfWorkingDays+"\nWage per Hour => "+wagePerHour+"\nWorking Hours Per Month => "+workingHoursPerMonth;
-    }
-
     public static void main(String[] args){
         System.out.println("**********************************Welcome to Employee Computation program**********************************\n");
 
         List<EmpWageBuilder> arraylist = new ArrayList<>();
 
-        EmployeeWageCalculation philips = new EmployeeWageCalculation("Philips",22,247.43,176);
-        EmployeeWageCalculation jio = new EmployeeWageCalculation("Jio",20,198.54,165);
-        EmployeeWageCalculation cognizant = new EmployeeWageCalculation("Cognizant",15,160.78,160);
+        EmpWageBuilder philips = new EmpWageBuilder("Philips",22,247.43,176);
+        EmpWageBuilder jio = new EmpWageBuilder("Jio",20,198.54,165);
+        EmpWageBuilder cognizant = new EmpWageBuilder("Cognizant",15,160.78,160);
 
         arraylist.add(philips);
         arraylist.add(jio);
@@ -102,17 +90,9 @@ public class EmployeeWageCalculation extends EmpWageBuilder{
         //Employee Attendance
         checkAttendance();
 
-        //Given Details
-        System.out.println("Given details of Companies...................");
-        for(EmpWageBuilder details:arraylist){
-            System.out.println("***************************************************************");
-            System.out.println(details);
-        }
-        System.out.println("***************************************************************");
-
         //Employee wage calculation
-        System.out.println("Total Salary = "+philips.calculateWage(arraylist,0)+"\n");
-        System.out.println("Total Salary = "+jio.calculateWage(arraylist,1)+"\n");
-        System.out.println("Total salary = "+cognizant.calculateWage(arraylist,2)+"\n");
+        philips.calculateWage(arraylist,0);
+        jio.calculateWage(arraylist,1);
+        cognizant.calculateWage(arraylist,2);
     }
 }
